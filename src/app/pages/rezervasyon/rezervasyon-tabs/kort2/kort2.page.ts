e4aade5550926253930900704a65054f84adcc69
YuksD <yuks26@outlook.com>
2024-05-28 16:26:09 +0300

    rezervasyon sayfasının icerigi eklendi

--- src/app/pages/rezervasyon/rezervasyon-tabs/kort2/kort2.page.ts
+++ src/app/pages/rezervasyon/rezervasyon-tabs/kort2/kort2.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kort2.page.scss'],
 })
 export class Kort2Page implements OnInit {
-
-  constructor() { }
+  timeSlots: { time: string, isAvailable: boolean }[] = [];
+  
+  constructor() { 
+    this.generateTimeSlots();
+  }
 
   ngOnInit() {
   }
+ generateTimeSlots() {
+    const startHour = 9;
+    const endHour = 21;
+    for (let hour = startHour; hour < endHour; hour++) {
+      this.timeSlots.push({ time: `${hour}:00`, isAvailable: this.checkAvailability(hour, 0) });
+      this.timeSlots.push({ time: `${hour}:30`, isAvailable: this.checkAvailability(hour, 30) });
+    }
+  }
 
+  checkAvailability(hour: number, minute: number): boolean {
+    // Doluluk kontrolünü burada gerçekleştirin
+    // Örnek olarak rastgele bir durum döndürüyoruz
+    return Math.random() > 0.5;
+  }
 }
+
