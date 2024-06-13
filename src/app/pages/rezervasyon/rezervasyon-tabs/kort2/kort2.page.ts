bc92aa1eaacbb71ebe6075c75bdc1ea6a24a6687
YuksD <yuks26@outlook.com>
2024-06-13 21:03:07 +0300

    bos olan kortlar artik tiklanilabilir ve modallar aktif

--- src/app/pages/rezervasyon/rezervasyon-tabs/kort2/kort2.page.ts
+++ src/app/pages/rezervasyon/rezervasyon-tabs/kort2/kort2.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { KortService } from 'src/app/services/kort.service';
+import { RezervasyonModalComponent } from 'src/app/pages/rezervasyon/rezervasyon-modal/rezervasyon-modal.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-kort2',
@@ -9,12 +11,26 @@ import { KortService } from 'src/app/services/kort.service';
 export class Kort2Page implements OnInit {
   timeSlots: { time: string, isAvailable: boolean }[] = [];
 
-  constructor(private kortService: KortService) { }
-
+  constructor(
+    private modalController: ModalController,
+    private kortService: KortService
+  ) { }
   ngOnInit() {
     // Bu bileşen artık kendi slotlarını yüklemiyor
     this.kortService.allSlots$.subscribe(slots => {
       this.timeSlots = slots.filter(slot => slot.kort === 2);
     });
+  }
+
+  async onBadgeClick(slot: any) {
+    if (slot.isAvailable) {
+      const modal = await this.modalController.create({
+        component: RezervasyonModalComponent,
+        cssClass: 'custom-modal'
+      });
+      await modal.present();
+    } else {
+      console.log('Slot dolu, rezervasyon yapılamaz.');
+    }
   }
 }
--- src/app/pages/rezervasyon/rezervasyon-tabs/kort3/kort3.page.ts
+++ src/app/pages/rezervasyon/rezervasyon-tabs/kort3/kort3.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { RezervasyonModalComponent } from 'src/app/pages/rezervasyon/rezervasyon-modal/rezervasyon-modal.component';
 import { KortService } from 'src/app/services/kort.service';
 
 @Component({
@@ -9,12 +11,27 @@ import { KortService } from 'src/app/services/kort.service';
 export class Kort3Page implements OnInit {
   timeSlots: { time: string, isAvailable: boolean }[] = [];
 
-  constructor(private kortService: KortService) { }
+  constructor(
+    private modalController: ModalController,
+    private kortService: KortService
+  ) { }
 
   ngOnInit() {
     // Bu bileşen artık kendi slotlarını yüklemiyor
     this.kortService.allSlots$.subscribe(slots => {
       this.timeSlots = slots.filter(slot => slot.kort === 3);
     });
+  }
+
+  async onBadgeClick(slot: any) {
+    if (slot.isAvailable) {
+      const modal = await this.modalController.create({
+        component: RezervasyonModalComponent,
+        cssClass: 'custom-modal'
+      });
+      await modal.present();
+    } else {
+      console.log('Slot dolu, rezervasyon yapılamaz.');
+    }
   }
 }
